7da95e9b594d703b1c0767dfc179846788271ffd
muhammedtahirakdeniz <muhammedtahirakdeniz@gmail.com>
2024-06-01 15:28:30 +0000

    added engin-site

--- firebase.js
+++ firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc, onSnapshot } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlkTsfbSEtr_MN0CVDkA1e6erzdCZF7qs",
-    authDomain: "ecommercewebsite-81922.firebaseapp.com",
-    projectId: "ecommercewebsite-81922",
-    storageBucket: "ecommercewebsite-81922.appspot.com",
-    messagingSenderId: "193954300185",
-    appId: "1:193954300185:web:666d3f5758223750ac0588",
-    measurementId: "G-36S4X45NLQ",
-  };
+  apiKey: "AIzaSyBlkTsfbSEtr_MN0CVDkA1e6erzdCZF7qs",
+  authDomain: "ecommercewebsite-81922.firebaseapp.com",
+  databaseURL: "https://ecommercewebsite-81922-default-rtdb.firebaseio.com",
+  projectId: "ecommercewebsite-81922",
+  storageBucket: "ecommercewebsite-81922.appspot.com",
+  messagingSenderId: "193954300185",
+  appId: "1:193954300185:web:11aff366a2d5012cac0588",
+  measurementId: "G-TV00F85X4W"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
